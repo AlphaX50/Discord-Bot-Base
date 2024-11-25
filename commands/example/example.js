0bc5267f9871100fbb6efa58649ce85c39eb8d7a
AlphaX50 <nazonware@gmail.com>
2024-11-25 18:34:03 +0100

    Update example.js

--- commands/example/example.js
+++ commands/example/example.js
@@ -3,7 +3,6 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('example') // command name
-        .setURL('https://discord.js.org/')
         .setDescription('An example of a simple order.'), // command description
     async execute(interaction) {
         await interaction.reply('This is an answer to the /example command !'); // bot response
