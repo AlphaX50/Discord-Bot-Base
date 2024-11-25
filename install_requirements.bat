afb8a59d267983170c4097d7c761ccb927e2b852
AlphaX50 <nazonware@gmail.com>
2024-11-25 13:51:00 +0100

    Add files via upload

---
+++ install_requirements.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Installing required modules...
+npm install discord.js
+echo Modules installed successfully!
+pause
---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Starting the Discord bot...
+node deploy-commands.js
+node index.js
+echo Bot stopped. Press any key to exit.
+pause
